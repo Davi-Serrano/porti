c8e9ea086ca060613c04acd50fd65d6a00fd14fa
Davi Serrano <daviscardoso2901@gmail.com>
2022-05-20 11:21:05 -0300

    home editada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,36 @@
-
-import {  Flex, Text } from '@chakra-ui/react'
-
+import {  Box ,Flex, Text } from '@chakra-ui/react'
+import { SlidePage } from "../components/Slide"
 
 export default function Home() {
    return (
-    <Flex
-      flexDir="column"
-      justify="center"
-      align="center"
-    >
-      <Text fontSize={30}> NextAPP</Text>
+    <Box>
+         <SlidePage /> 
+         <Box
+            background="#141414"
+            p="1em"
+          >
+            <Text as="h3">Sobre</Text>
+            <Text as="p">
+               Lorem Ipsum is simply dummy text of
+               the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+               the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+             </Text>
+         </Box>
+         
+      
+          <Box
+            background="#141414"
+            p="1em"
+          >
+            <Text as="h3">Stacks</Text>
+            <Box 
+               h="80px"
+               w="80%"
+             >  
+         
+            </Box>
+         </Box>
 
-    </Flex>
+    </Box>
   )
 }
