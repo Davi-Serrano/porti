720fd0144a0c39f63fb75030aede465c5c4bec55
Davi Serrano <daviscardoso2901@gmail.com>
2022-05-20 09:51:21 -0300

    header add a todas as páginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/theme.ts'
 import type { AppProps } from 'next/app'
 import { ChakraProvider} from "@chakra-ui/react" 
 import { theme } from '../styles/theme'
+import { Header } from "../components/Header"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS={false} theme={theme}>
+          <Header />
           <Component {...pageProps} />
  
     </ChakraProvider>   
