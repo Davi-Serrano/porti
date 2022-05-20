9edbab19407f93d52665c3eef1ab5673793fd271
Davi Serrano <daviscardoso2901@gmail.com>
2022-05-19 21:58:13 -0300

    config projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,12 @@ import '../styles/theme.ts'
 import type { AppProps } from 'next/app'
 import { ChakraProvider} from "@chakra-ui/react" 
 import { theme } from '../styles/theme'
-import { Header } from '../components/Header'
-import CoinsProvider from "../context/coins"
-import {SessionProvider } from "next-auth/react"
-import NextProgess from "nextjs-progressbar"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS={false} theme={theme}>
-      <SessionProvider session={pageProps.session}>
-        <CoinsProvider>
-          <Header />
           <Component {...pageProps} />
-          <NextProgess />
-        </CoinsProvider>
-      </SessionProvider>
+ 
     </ChakraProvider>   
   )
 }
