e879da3742c428ddb7788a25f372b8c7ddc6de72
Davi Serrano <daviscardoso2901@gmail.com>
2022-07-13 08:54:29 -0300

    fotos do curso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,11 +32,9 @@ export default function Home() {
             ml="18%"
           >
             <Text as="h2">Stacks</Text>
-            <Text as="p">
-               Lorem Ipsum is simply dummy text of
-               the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
-               the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-             </Text>
+            <Flex>
+                  <Image src="../../public/photos/javascript.svg"
+             </Flex>
          </Box>
 
          <Box
