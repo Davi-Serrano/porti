a5cf35cc2df0df1ea81d61c091f8e7355af4ed3d
Davi Serrano <daviscardoso2901@gmail.com>
2022-08-30 22:58:08 -0300

    seção de projetos criadas

--- src/pages/projetos.tsx
+++ src/pages/projetos.tsx
@@ -3,64 +3,81 @@ import { Flex, Box, Text, Image, Input } from '@chakra-ui/react'
 export default function Contanto(){
     return(
         <Flex
+            justify="space-around"
+            align="center"
+            flexWrap="wrap"
             mx="auto"
             mt="2em"
-            w="80%"
-            color="#C5C5C5"
+            w="100%"
+            color="#000"
         >
             <Flex 
                 flexDir="column"
                 justify="space-around"
                 align="center"
+                border="1px solid red"
+                p=".5em"
+                m="2em"
             >
                 <Image 
+                    src="./react.jpg"
                     w="90%"
                     alt="imagem do projeto"
                 />
 
-                <Box>
-                    <Text>NextJs</Text>
-                    <Text>TypeScript</Text>
-                    <Text>FaunaDB</Text>
-                    <Text>ChakraUI</Text>
-                </Box>
+                <Flex flexWrap="wrap"  maxW="350px">
+                    <Text textAlign="center" w="50%">NextJs</Text>
+                    <Text textAlign="center" w="50%">TypeScript</Text>
+                    <Text textAlign="center" w="50%">FaunaDB</Text>
+                    <Text textAlign="center" w="50%">ChakraUI</Text>
+                    <Text textAlign="center" w="100%">Ver mais detalhes</Text>
+                </Flex>
             </Flex>
 
             <Flex 
                 flexDir="column"
                 justify="space-around"
                 align="center"
+                border="1px solid red"
+                p=".5em"
+                m="2em"
             >
                 <Image 
-                    src=""
+                    src="./react.jpg"
                     w="90%"
                     alt="imagem do projeto"
                 />
 
-                <Box>
-                    <Text>NextJs</Text>
-                    <Text>TypeScript</Text>
-                    <Text>FaunaDB</Text>
-                    <Text>ChakraUI</Text>
-                </Box>
+                <Flex flexWrap="wrap"  maxW="350px">
+                    <Text textAlign="center" w="50%">NextJs</Text>
+                    <Text textAlign="center" w="50%">TypeScript</Text>
+                    <Text textAlign="center" w="50%">FaunaDB</Text>
+                    <Text textAlign="center" w="50%">ChakraUI</Text>
+                    <Text textAlign="center" w="100%">Ver mais detalhes</Text>
+                </Flex>
             </Flex>
 
             <Flex 
                 flexDir="column"
                 justify="space-around"
                 align="center"
+                border="1px solid red"
+                p=".5em"
+                m="2em"
             >
                 <Image 
+                    src="./react.jpg"
                     w="90%"
                     alt="imagem do projeto"
                 />
 
-                <Box>
-                    <Text>NextJs</Text>
-                    <Text>TypeScript</Text>
-                    <Text>FaunaDB</Text>
-                    <Text>ChakraUI</Text>
-                </Box>
+                <Flex flexWrap="wrap"  maxW="350px">
+                    <Text textAlign="center" w="50%">NextJs</Text>
+                    <Text textAlign="center" w="50%">TypeScript</Text>
+                    <Text textAlign="center" w="50%">FaunaDB</Text>
+                    <Text textAlign="center" w="50%">ChakraUI</Text>
+                    <Text textAlign="center" w="100%">Ver mais detalhes</Text>
+                </Flex>
             </Flex>
         </Flex>
     )
